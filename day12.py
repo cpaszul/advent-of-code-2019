166ee2e4eae8d40d1ed342d82000eedc7744fe9c
Cory Paszul <cpaszul@gmail.com>
2019-12-12 14:46:30 -0500

    Improved cycle dectection

--- day12.py
+++ day12.py
@@ -58,34 +58,28 @@ def part_2(loc=DEFAULT_INPUT):
     with open(loc) as f:
         lines = [line for line in f.readlines()]
     moons = [Moon(*map(int, moon_re.findall(line))) for line in lines]
-    xs = {}
-    ys = {}
-    zs = {}
-    i = 0
+    initial_x = tuple((moon.x, moon.vx) for moon in moons)
+    initial_y = tuple((moon.y, moon.vy) for moon in moons)
+    initial_z = tuple((moon.z, moon.vz) for moon in moons)
+    i = 1
     x_repeat = y_repeat = z_repeat = False
     while not (x_repeat and y_repeat and z_repeat):
+        increment(moons)
         if not x_repeat:
             x_state = tuple((moon.x, moon.vx) for moon in moons)
-            if x_state in xs:
-                x_period = i - xs[x_state]
+            if x_state == initial_x:
+                x_period = i
                 x_repeat = True
-            else:
-                xs[x_state] = i
         if not y_repeat:
             y_state = tuple((moon.y, moon.vy) for moon in moons)
-            if y_state in ys:
-                y_period = i - ys[y_state]
+            if y_state == initial_y:
+                y_period = i
                 y_repeat = True
-            else:
-                ys[y_state] = i
         if not z_repeat:
             z_state = tuple((moon.z, moon.vz) for moon in moons)
-            if z_state in zs:
-                z_period = i - zs[z_state]
+            if z_state == initial_z:
+                z_period = i
                 z_repeat = True
-            else:
-                zs[z_state] = i
-        increment(moons)
         i += 1
     return lcm(x_period, y_period, z_period)
 
